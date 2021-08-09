bc2340771b705c9661ece92d9301167ac0a838ee
Shun Yuen <shyuen@ualberta.net>
2021-08-09 16:24:13 -0600

    add customized Pion

---
+++ custom-pion/main.go
@@ -0,0 +1,120 @@
+/*
+This is based on Pion examples
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"regexp"
+	"strconv"
+	"syscall"
+
+	"github.com/pion/stun"
+	"github.com/pion/turn/v2"
+)
+
+// stunLogger wraps a PacketConn and prints incoming/outgoing STUN packets
+// This pattern could be used to capture/inspect/modify data as well
+type stunLogger struct {
+	net.PacketConn
+}
+
+func (s *stunLogger) WriteTo(p []byte, addr net.Addr) (n int, err error) {
+	if n, err = s.PacketConn.WriteTo(p, addr); err == nil && stun.IsMessage(p) {
+		msg := &stun.Message{Raw: p}
+		if err = msg.Decode(); err != nil {
+			return
+		}
+
+		fmt.Printf("Outbound STUN: %s \n", msg.String())
+	}
+
+	return
+}
+
+func (s *stunLogger) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	if n, addr, err = s.PacketConn.ReadFrom(p); err == nil && stun.IsMessage(p) {
+		msg := &stun.Message{Raw: p}
+		if err = msg.Decode(); err != nil {
+			return
+		}
+
+		fmt.Printf("Inbound STUN: %s \n", msg.String())
+	}
+
+	return
+}
+
+func main() {
+	publicIP := flag.String("public-ip", "", "IP Address that TURN can be contacted by.")
+	port := flag.Int("port", 3478, "Listening port.")
+	users := flag.String("users", "", "List of username and password (e.g. \"user=pass,user=pass\")")
+	realm := flag.String("realm", "ScopeAR", "Realm (defaults to \"ScopeAR\")")
+	minPort := flag.Int("port-range-min", 49152, "lower bounds of the UDP relay endpoints (default is 49152).")
+	maxPort := flag.Int("port-range-max", 65535, "upper bounds of the UDP relay endpoints (default is 65535).")
+	flag.Parse()
+
+	if len(*publicIP) == 0 {
+		log.Fatalf("'public-ip' is required")
+	} else if len(*users) == 0 {
+		log.Fatalf("'users' is required")
+	}
+
+	// Create a UDP listener to pass into pion/turn
+	// pion/turn itself doesn't allocate any UDP sockets, but lets the user pass them in
+	// this allows us to add logging, storage or modify inbound/outbound traffic
+	udpListener, err := net.ListenPacket("udp4", "0.0.0.0:"+strconv.Itoa(*port))
+	if err != nil {
+		log.Panicf("Failed to create TURN server listener: %s", err)
+	}
+
+	// Cache -users flag for easy lookup later
+	// If passwords are stored they should be saved to your DB hashed using turn.GenerateAuthKey
+	usersMap := map[string][]byte{}
+	for _, kv := range regexp.MustCompile(`(\w+)=(\w+)`).FindAllStringSubmatch(*users, -1) {
+		usersMap[kv[1]] = turn.GenerateAuthKey(kv[1], *realm, kv[2])
+	}
+
+	s, err := turn.NewServer(turn.ServerConfig{
+		Realm: *realm,
+		// Set AuthHandler callback
+		// This is called everytime a user tries to authenticate with the TURN server
+		// Return the key for that user, or false when no user is found
+		AuthHandler: func(username string, realm string, srcAddr net.Addr) ([]byte, bool) {
+			if key, ok := usersMap[username]; ok {
+				return key, true
+			}
+			return nil, false
+		},
+		// PacketConnConfigs is a list of UDP Listeners and the configuration around them
+		PacketConnConfigs: []turn.PacketConnConfig{
+			{
+				PacketConn: &stunLogger{udpListener}, // Enabled logging output
+				RelayAddressGenerator: &turn.RelayAddressGeneratorPortRange{
+					RelayAddress: net.ParseIP(*publicIP), // Claim that we are listening on IP passed by user (This should be your Public IP)
+					Address:      "0.0.0.0",              // But actually be listening on every interface
+					MinPort:      uint16(*minPort),
+					MaxPort:      uint16(*maxPort),
+				},
+			},
+		},
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Block until user sends SIGINT or SIGTERM
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+
+	if err = s.Close(); err != nil {
+		log.Panic(err)
+	}
+}
